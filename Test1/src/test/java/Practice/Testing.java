8bffa0b23751360dc0d13e09ceaec19c1fff7504
D E L L <D E L L@DESKTOP-EH3C29F>
2025-07-28 17:16:19 +0530

    commit done

---
+++ Test1/src/test/java/Practice/Testing.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class Testing {
+
+	public static void main(String[] args) {
+
+		System.out.println("Testing completed");
+		System.out.println("Testing completed");
+		System.out.println("Testing  not completed");
+		System.out.println("Testing  not completed");
+		
+
+	}
+
+}
